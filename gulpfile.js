8ed43016a8e620c71e64f6d2ba8f6e5c87dffbe8
Firas yangui <fyangui@linagora.com>
2017-12-14 12:04:12 +0100

    modify dep

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp'),
-		$ = require('gulp-load-plugins')();
+		$ = require('gulp-load-plugins')(),
+		//stylish = require('jshint-stylish'),
+		karma = require('karma');
 
-var scripts = ['js/**/*.js']
+var scripts = ['js/**/*.js','!js/spec/*.js'];
+var karmaFiles = ['node_modules/angular/angular.js',
+									'node_modules/jquery/dist/jquery.min.js',
+									'node_modules/angular-route/angular-route.js',
+									'node_modules/angular-mocks/angular-mocks.js',
+									'node_modules/bootstrap/dist/js/bootstrap.min.js'
+								];
+var specs = 'js/spec/*.js';
+var bundle = 'bin/js/**/*.js';
+
 gulp.task('scripts', function() {
 	gulp.src(scripts)
 		.pipe($.babel({
@@ -15,6 +26,20 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('bin/js'));
 });
 
-gulp.task('default', ['scripts'], function() {
-	gulp.watch(scripts, ['scripts'])
+gulp.task('default', ['test'], function() {
+	gulp.watch(scripts, ['test'])
+});
+
+gulp.task('test', ['scripts'], function(done) {
+
+	var files = [specs, bundle];
+	files = karmaFiles.concat(files);
+
+	new karma.Server({
+		configFile: __dirname + '/karma.conf.js',
+		files: files,
+		singleRun: true
+	}, function() {
+		done();
+	}).start();
 });
